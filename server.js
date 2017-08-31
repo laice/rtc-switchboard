1a7e81a67deb094a973e5c4edc847af0daed1ea6
jynn <pshenkyr@gmail.com>
2017-08-31 07:41:44 -0600

    options and ssl

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
-var server = require('http').createServer();
+let config = require('config.json');
+
+
+var server = require('https').createServer({
+    key: config.key,
+    cert: config.cert
+});
 var switchboard = require('./')(server, { servelib: true });
-var port = parseInt(process.env.NODE_PORT || process.env.PORT || process.argv[2], 10) || 3000;
+var port = parseInt(process.env.NODE_PORT || process.env.PORT || process.argv[2], 10) || 8444;
 var host = process.env.NODE_HOST || process.env.HOST || 'localhost';
-// var replify = require('replify');
+var replify = require('replify');
 
 server.on('request', function(req, res) {
-  if (req.url === '/') {
-    res.writeHead(302, {
-      'Location': 'https://github.com/rtc-io/rtc-switchboard'
-    });
-    res.end('switchboard available from: https://github.com/rtc-io/rtc-switchboard');
-  }
+  // if (req.url === '/') {
+  //   res.writeHead(302, {
+  //     'Location': 'https://github.com/rtc-io/rtc-switchboard'
+  //   });
+  //   res.end('switchboard available from: https://github.com/rtc-io/rtc-switchboard');
+  // }
 });
 
 // start the server
@@ -22,24 +28,24 @@ server.listen(port, host, function(err) {
   console.log('server running at http://' + host + ':' + port + '/');
 });
 
-// // add the repl
-// replify({
-//   name: 'switchboard',
-//   app: switchboard,
-//   contexts: {
-//     server: server
-//   }
-// });
-//
-// switchboard.on('room:create', function(room) {
-//   console.log('room ' + room + ' created, now have ' + switchboard.rooms.length + ' active rooms');
-// });
-//
-// switchboard.on('room:destroy', function(room) {
-//   console.log('room ' + room + ' destroyed, ' + switchboard.rooms.length + ' active rooms remain');
-//
-//   if (typeof gc == 'function') {
-//     console.log('gc');
-//     gc();
-//   }
-// });
+// add the repl
+replify({
+  name: 'switchboard',
+  app: switchboard,
+  contexts: {
+    server: server
+  }
+});
+
+switchboard.on('room:create', function(room) {
+  console.log('room ' + room + ' created, now have ' + switchboard.rooms.length + ' active rooms');
+});
+
+switchboard.on('room:destroy', function(room) {
+  console.log('room ' + room + ' destroyed, ' + switchboard.rooms.length + ' active rooms remain');
+
+  if (typeof gc == 'function') {
+    console.log('gc');
+    gc();
+  }
+});
